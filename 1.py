9525d0faae023d94f0f456d3a5381e78d50d4663
AL KAFI SOHAG <85807115+al-kafi-sohag@users.noreply.github.com>
2026-05-17 11:49:54 +0600

    Added a new print end line

--- 1.py
+++ 1.py
@@ -4,3 +4,4 @@ number = 1
 while number <= 20:
     print(number)
     number += 1
+print("----------------------------------- End Print numbers from 1 to 20 using while loop--------------------------")

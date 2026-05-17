d57d3da38fc8520643c9dae4f3e369da00baaaf9
AL KAFI SOHAG <85807115+al-kafi-sohag@users.noreply.github.com>
2026-05-17 11:47:41 +0600

    Print numbers from 1 to 20 using while loop

---
+++ 1.py
@@ -0,0 +1,6 @@
+# 1. Print numbers from 1 to 20 using while loop
+print("-----------------------------------Print numbers from 1 to 20 using while loop--------------------------")
+number = 1
+while number <= 20:
+    print(number)
+    number += 1

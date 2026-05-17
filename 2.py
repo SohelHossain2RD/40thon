d36163de675a1cb934ccd96a0af51d93340bae06
AL KAFI SOHAG <85807115+al-kafi-sohag@users.noreply.github.com>
2026-05-17 12:27:53 +0600

    Created 2 new files

---
+++ 2.py
@@ -0,0 +1,7 @@
+print("-----------------------------------Print numbers txt from  using while loop--------------------------")
+number = 1
+while number <= 20:
+    print(number, end=" ")
+    number += 1
+    numbers  ={"0"}
+    print(" numbers ")
